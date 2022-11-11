91f794d92f3cb6efc262008584f018c81a1e6ccc
Михаил <113143354+Marshal51rus@users.noreply.github.com>
2022-11-11 16:25:15 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+TOKEN = '5620450447:AAEmjm0N0DthReFHbrKGkYUOrwem1vGklgQ'
+API_access_key = "XT8WyDT0kI4GFLmRszQiKx11sue4HTYZ"
+
+payload = {}
+headers= {
+  "apikey": API_access_key
+}
+
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+}
